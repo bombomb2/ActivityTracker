6de56e5b3fabccc61622a5aa4de1768e3b9d78a3
yangachy <33654554+yangachy@users.noreply.github.com>
2018-06-20 01:17:01 +0900

    0.8.1
    다산홀 입력

--- app/src/main/java/com/dasom/activitytracker/IndoorService.java
+++ app/src/main/java/com/dasom/activitytracker/IndoorService.java
@@ -98,7 +98,7 @@ public class IndoorService extends Service {
 
         //각 위치의 ap정보를 객체로 생성
         location[0] = new Location_in("401호 계단 앞", "64:e5:99:db:05:cc", -60, "18:80:90:c6:7b:20", -61, "18:80:90:c6:7b:22", -60);
-        location[1] = new Location_in("방", "64:e5:99:8e:4c:58", -49, "00:40:5a:e4:b2:fb", -52, "64:e5:99:8e:4c:58", -52);
+        location[1] = new Location_in("다산홀", " 20:3a:07:9e:a6:c0", -66, "20:3a:07:9e:a6:c5", -62, "a4:18:75:58:77:d1", -58);
     }
 
     @Override
